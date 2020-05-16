28cbed7c0c5f7b809291e13b6490263f0f3279c2
Ebnezer <58810515+Abanazar@users.noreply.github.com>
2020-05-16 17:11:09 +0300

    Update design.py

--- design.py
+++ design.py
@@ -1,12 +1,4 @@
 # -*- coding: utf-8 -*-
-
-# Form implementation generated from reading ui file 'untui.ui'
-#
-# Created by: PyQt5 UI code generator 5.14.2
-#
-# WARNING! All changes made in this file will be lost!
-
-
 from PyQt5 import QtCore, QtGui, QtWidgets
 
 
